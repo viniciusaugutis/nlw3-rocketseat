4bec7dd265a35197847e5444dca55bbe0d80a660
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-14 20:37:55 -0300

    Configurando typeorm e criando migration

---
+++ backend/anotacoes.md
@@ -0,0 +1,3 @@
+yarn typeorm migration:create -n create_orphanages
+
+Criar migration
---
+++ backend/src/database/migrations/1602718648871-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602718648871 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
