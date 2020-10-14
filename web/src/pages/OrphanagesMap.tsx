7eb2b734e159089a367e29e2114738811689e149
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-13 22:44:10 -0300

    Criando rota para mapa da aplicacao

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import mapMarkerImg from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanage-map.css';
+
+import { Map, TileLayer } from 'react-leaflet';
 
 function OrphanagesMap() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Londrina</strong>
+          <span>PR</span>
+        </footer>
+      </aside>
+
+      <div>
+        <Map
+          center={[-23.3146145, -50.0701201]}
+          zoom={15}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </Map>
+      </div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff"></FiPlus>
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
