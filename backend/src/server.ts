121c3e4659ae394e4ea29f05c75ac79f5c559ae6
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-14 21:03:18 -0300

    Salvando um orfanato

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,36 @@
 import express from 'express';
 import './database/connection';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
   return response.send({ message: 'Text' });
 });
 
